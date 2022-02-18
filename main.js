f6afc68fd9a2e89d68d71cc3590b5bd14aeb52b9
wes-mitchell <wmdrums@gmail.com>
2022-02-18 08:50:02 -0600

    initial commit file for exercise

---
+++ main.js
@@ -0,0 +1,15 @@
+const addTwoNumbers = (num1, num2) => {
+  return num1 + num2
+}
+
+const multiplyByTwo = (anyNumber) => {
+  return anyNumber * 2
+}
+
+let four = addTwoNumbers(2, 2)
+
+console.log(four)
+
+let byTwo = multiplyByTwo(16)
+
+console.log(byTwo)
